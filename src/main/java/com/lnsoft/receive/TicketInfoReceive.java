0a013edc2b3f794774775ef940bcc85f66eb072b
cyl5yy412 <cyl5yy412@gmail.com>
2019-04-10 10:55:19 +0800

    nn

--- src/main/java/com/lnsoft/receive/TicketInfoReceive.java
+++ src/main/java/com/lnsoft/receive/TicketInfoReceive.java
@@ -37,9 +37,10 @@ public class TicketInfoReceive {
             //告诉服务器收到这条消息 已经被我消费了 可以在队列删掉 这样以后就不会再发了 否则消息服务器以为这条消息没处理掉 后续还会在发
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); // false只确认当前一个消息收到，true确认所有consumer获得的消息
 
-        } catch (Exception e) {
-            e.printStackTrace();
 
+
+
+            //#######################################################
             if (message.getMessageProperties().getRedelivered()) {//该消息已经被消费
                 System.out.println("消息已重复处理失败,拒绝再次接收...");
                 channel.basicReject(message.getMessageProperties().getDeliveryTag(), true); // 拒绝消息
@@ -47,6 +48,10 @@ public class TicketInfoReceive {
                 System.out.println("消息即将再次返回队列处理...");
                 channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true); // requeue为是否重新回到队列
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+
 
         }
     }
